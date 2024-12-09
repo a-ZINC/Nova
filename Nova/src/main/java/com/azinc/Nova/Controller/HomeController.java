006ba17a23d047d1021b607495c3414090346d0b
a-ZINC <azinc28@gmail.com>
2024-12-10 02:50:40 +0530

    added thymeleaf

---
+++ Nova/src/main/java/com/azinc/Nova/Controller/HomeController.java
@@ -0,0 +1,15 @@
+package com.azinc.Nova.Controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HomeController {
+    
+    @RequestMapping(value = {"", "/", "home"})
+    public String home(Model model) {
+        model.addAttribute("username", "azinc");
+        return "home";
+    }
+}
---
+++ Nova/src/test/java/com/azinc/Nova/NovaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.azinc.Nova;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NovaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
